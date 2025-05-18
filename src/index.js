3e6da4ab849daedb6a84c91c57ec52cb70df34c6
Kaleb-Espinel <kalebespinel.09@outlook.es>
2025-05-18 18:21:14 -0500

    Mi primer commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import react from "react";
+import ReactDOM from "react-dom/client";
+import Component from "./input.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <>
+    <h2>CUAL ES EL AREA DE UN TRIANGULO DE BASE 12 CM Y ALTURA 9CM?</h2>
+
+    <Component />
+  </>
+);
---
+++ src/input.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Component() {
+  const [inputValor, setInputValor] = useState("");
+
+  const OnChangeInput = (event) => {
+    setInputValor(event.target.value);
+  };
+
+  const ClickButton = () => {
+    return <p>{inputValor === "54 cm2" ? "Correcto" : "Incorrecto"}</p>;
+  };
+
+  const handleClick = () => {
+    const valueToUse = inputValor;
+  };
+
+  return (
+    <>
+      <input type="text" value={inputValor} onChange={OnChangeInput}></input>
+      <button onClick={ClickButton}>COMPROBAR</button>
+      <p>Tu respuesta es: </p>
+      <ClickButton/>
+    </>
+  );
+}
+export default Component;
